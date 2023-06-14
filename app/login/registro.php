90c569bd1465af9c031f3943150d477c79a9a167
gmoli <gmoli.it@gmail.com>
2023-06-14 11:41:34 -0300

    subiendo cambios

--- app/login/registro.php
+++ app/login/registro.php
@@ -17,7 +17,7 @@ function debug_to_console($data) {
    //$db_connection = mysql_connect($db_host, $db_user, $db_password);
 
 if (!$db_connection) {
-	die('No se ha podido conectar a la base de datos');
+	echo 'No se ha podido conectar a la base de datos' ;die();
 }
 $subs_name = utf8_decode($_POST['nombre']);
 //$subs_last = utf8_decode($_POST['apellido']);
